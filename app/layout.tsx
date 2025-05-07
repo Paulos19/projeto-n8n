a2702229c21fdebf434f99a43970b01040ce1622
Paulos19 <paulohenrique.012araujo@gmail.com>
2025-05-07 15:53:08 -0300

    vamos-lá

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider"; // Importe o ThemeP
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "R.A.I.O - Análise Inteligente de Avaliações",
+  title: "R.A.I.O | Análise Inteligente de Avaliações",
   description: "Plataforma de IA para análise e gestão de feedback de clientes.",
 };
 
